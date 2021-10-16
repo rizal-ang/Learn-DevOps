97fe0cf7c28c4fedf7a758838180516612687757
Rizal Ang <rizal.ang@avanade.com>
2021-10-16 10:52:09 +0100

    fix

--- Platform/Terraform-Advanced/providers.tf
+++ Platform/Terraform-Advanced/providers.tf
@@ -1,6 +1,9 @@
 terraform {
-  backend "azurerm" {}
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
     databricks = {
       source = "databrickslabs/databricks"    
     }
--- Platform/Terraform-Basic/providers.tf
+++ Platform/Terraform-Basic/providers.tf
@@ -1,3 +1,12 @@
 terraform {
-  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
