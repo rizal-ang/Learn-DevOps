88a17baf6884e688f7cf38364c1f51849d02f542
Rizal Ang <rizal.ang@avanade.com>
2021-10-19 19:07:50 +0100

    new files

---
+++ Platform/Database/dbo/Location.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [staging].[Location](
+	[SITEID] [nvarchar](100) NULL,
+	[TURBINENO] [nvarchar](500) NOT NULL,
+	[ISACTIVE] [char](1) NULL,
+	[CREATEDBY] [nvarchar](100) NULL,
+	[CREATEDDATE] [datetime] NULL,
+	[LASTMODIFIEDBY] [nvarchar](100) NULL,
+	[LASTMODIFIEDDATE] [datetime] NULL
+) ON [PRIMARY]
